3cc15deb29c415ad1a956e9a70dfd89bad510100
ARSALAN <ARSALAN@DESKTOP-Q28VCLK>
2017-03-27 02:14:27 +0500

    Chain Of Responsibility

---
+++ ChainOfResponsibility/src/INumber.java
@@ -0,0 +1,7 @@
+
+public interface INumber {
+	public void cal(Number num);
+	public void next(INumber next);
+	
+
+}
---
+++ ChainOfResponsibility/src/Number.java
@@ -0,0 +1,25 @@
+
+public class Number {
+	
+	private int num1;
+	private int num2;
+	private String value;
+	public Number(int a, int b, String c){
+		
+		
+		this.num1=a;
+		this.num2=b;
+		this.value=c;
+				
+	}
+	public int getnum1(){
+		return num1;
+	}
+	public int getnum2(){
+		return num2;
+	}
+	public String getvalue (){
+		return value;
+	}
+
+}
